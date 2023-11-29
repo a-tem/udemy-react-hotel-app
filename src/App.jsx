6969dbafdc76d211fda7ee04436d7b0bde399298
Artem Voloshyn <voloshin.artem.a@gmail.com>
2023-11-29 17:00:52 +0100

    refactor: Moved Button and Input styled components to the separate files in ./ui folder; Added rules to the eslintrc.cjs file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,13 @@
+import React from "react";
 import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
 
 // NOTE: To have the code highlighting and autocompletion as "normal CSS", install the vscode-styled-components extension
 const H1 = styled.h1`
   font-size: 40px;
   font-weight: 600;
-`;
-
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: var(--border-radius-sm);
-  background-color: var(
-    --color-brand-600
-  ); // NOTE: It's possible to use css variables in styledComponents
-  color: var(--color-brand-50);
-  box-shadow: var(--shadow-small);
-`;
-
-const Input = styled.input`
-  border: 1px solid var(--color-grey-300);
-  background-color: var(--color-grey-0);
-  border-radius: var(--border-radius-sm);
-  box-shadow: var(--shadow-small);
-  padding: 0.8rem 1.2rem;
 `;
 
 const StyledApp = styled.div`
