d1e3608a6b566767de6daa25d36a74705602e5a1
Artem Voloshyn <voloshin.artem.a@gmail.com>
2023-11-29 16:16:43 +0100

    chore: Added GlobalStyles.jsx (createGlobalStyles) styled component that contain application-wide global styling; Using css variables in styledComponents (App.jsx); Added GoogleFonts to the index.html file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
 
 // NOTE: To have the code highlighting and autocompletion as "normal CSS", install the vscode-styled-components extension
 const H1 = styled.h1`
@@ -11,14 +12,19 @@ const Button = styled.button`
   padding: 1.2rem 1.6rem;
   font-weight: 500;
   border: none;
-  border-radius: 7px;
-  background-color: purple;
-  color: white;
+  border-radius: var(--border-radius-sm);
+  background-color: var(
+    --color-brand-600
+  ); // NOTE: It's possible to use css variables in styledComponents
+  color: var(--color-brand-50);
+  box-shadow: var(--shadow-small);
 `;
 
 const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 5px;
+  border: 1px solid var(--color-grey-300);
+  background-color: var(--color-grey-0);
+  border-radius: var(--border-radius-sm);
+  box-shadow: var(--shadow-small);
   padding: 0.8rem 1.2rem;
 `;
 
@@ -29,11 +35,15 @@ const StyledApp = styled.div`
 
 function App() {
   return (
-    <StyledApp>
-      <H1>Styled text</H1>
-      <Button>Hey!</Button>
-      <Input />
-    </StyledApp>
+    <>
+      {/* NOTE: GlobalStyles component shoud not have any children, that's why use fragment around it and sibling StyledApp component */}
+      <GlobalStyles />
+      <StyledApp>
+        <H1>Styled text</H1>
+        <Button>Hey!</Button>
+        <Input />
+      </StyledApp>
+    </>
   );
 }
 export default App;
