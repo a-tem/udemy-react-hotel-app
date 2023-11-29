0e1943824168812f5cdcebed37e99e7836cc1794
Artem Voloshyn <voloshin.artem.a@gmail.com>
2023-11-29 17:28:22 +0100

    refactor: Moved and refactored H1 styled component to separate file, added accessing to the props and use it for dynamic styles.

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,9 @@ import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
+import Heading from "./ui/Heading";
 
 // NOTE: To have the code highlighting and autocompletion as "normal CSS", install the vscode-styled-components extension
-const H1 = styled.h1`
-  font-size: 40px;
-  font-weight: 600;
-`;
-
 const StyledApp = styled.div`
   background-color: orangered;
   padding: 20px;
@@ -21,8 +17,10 @@ function App() {
       {/* NOTE: GlobalStyles component shoud not have any children, that's why use fragment around it and sibling StyledApp component */}
       <GlobalStyles />
       <StyledApp>
-        <H1>Styled text</H1>
+        <Heading as="h1">App</Heading>
+        <Heading as="h2">Section</Heading>
         <Button>Hey!</Button>
+        <Heading as="h3">Form</Heading>
         <Input />
       </StyledApp>
     </>
